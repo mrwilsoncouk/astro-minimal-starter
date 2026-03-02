119361787bfbcf1e231e600d53a82777ef88139a
tomrcc <tom@cloudcannon.com>
2026-03-03 12:52:06 +1300

    Register PostHero. Clean up dependencies

--- src/scripts/register-components.ts
+++ src/scripts/register-components.ts
@@ -3,7 +3,9 @@ import "@cloudcannon/editable-regions/astro-react-renderer";
 import Hero from "../components/heroes/hero/hero.astro";
 import LeftRight from "../components/left-right/left-right.astro";
 import TextBlock from "../components/text-block/text-block.astro";
+import PostHero from "../components/blog/post-hero/post-hero.astro";
 
 registerAstroComponent("Hero", Hero);
 registerAstroComponent("LeftRight", LeftRight);
 registerAstroComponent("TextBlock", TextBlock);
+registerAstroComponent("PostHero", PostHero);
