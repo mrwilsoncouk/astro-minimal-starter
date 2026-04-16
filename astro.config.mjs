863a46cc385cd4edfffd91029948c60cb2e2cd57
mrwilsoncouk <info@mrwilson.co.uk>
2026-04-16 03:50:50 +0100

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,5 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import editableRegions from "@cloudcannon/editable-regions/astro-integration";
-import mdx from "@astrojs/mdx";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: "https://tiny-jackal.cloudvent.net/",
-  integrations: [react(), editableRegions(), mdx()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  site: 'https://example.com'
 });
